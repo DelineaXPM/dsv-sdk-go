2918df6da6593407a42a5baa91919aa9622839ae
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-06-12 14:59:29 +0200

    Implement CreateSecret and DeleteSecret operations
    Closes #33.

--- vault/secret.go
+++ vault/secret.go
@@ -22,7 +22,8 @@ type Secret struct {
 	secretResource
 }
 
-// Secret gets the secret at path from the DSV of the given tenant
+// Secret gets the secret at path from the DSV of the given tenant.
+// See https://dsv.secretsvaultcloud.com/api#operation/getSecret.
 func (v Vault) Secret(path string) (*Secret, error) {
 	data, err := v.accessResource(http.MethodGet, secretsResource, path, nil)
 	if err != nil {
@@ -32,6 +33,36 @@ func (v Vault) Secret(path string) (*Secret, error) {
 	secret := &Secret{}
 	if err := json.Unmarshal(data, secret); err != nil {
 		log.Printf("[DEBUG] error parsing response from /%s/%s: %q", secretsResource, path, data)
+		return nil, err
+	}
+	return secret, nil
+}
+
+// DeleteSecret deletes the secret at path from the DSV of the given tenant.
+// See https://dsv.secretsvaultcloud.com/api#operation/deleteSecret.
+func (v Vault) DeleteSecret(path string) error {
+	_, err := v.accessResource(http.MethodDelete, secretsResource, path, nil)
+	return err
+}
+
+// SecretRequest represents the request body of the CreateSecret operation.
+type SecretRequest struct {
+	Attributes map[string]interface{}
+	Data       map[string]interface{}
+	Description string
+}
+
+// CreateSecret creates the secret at path in the DSV of the given tenant.
+// See https://dsv.secretsvaultcloud.com/api#operation/createSecret.
+func (v Vault) CreateSecret(path string, req SecretRequest) (*Secret, error) {
+	d, err := v.accessResource(http.MethodPost, secretsResource, path, req)
+	if err != nil {
+		return nil, err
+	}
+
+	secret := &Secret{}
+	if err := json.Unmarshal(d, secret); err != nil {
+		log.Printf("[DEBUG] error parsing response from /%s/%s: %q", secretsResource, path, d)
 		return nil, err
 	}
 	return secret, nil
--- vault/secret_test.go
+++ vault/secret_test.go
@@ -18,3 +18,33 @@ func TestSecret(t *testing.T) {
 		t.Error("secret.Data is nil")
 	}
 }
+
+
+func TestCreateSecret(t *testing.T) {
+	secret, err := createSecret(secretName)
+
+	if err != nil {
+		t.Error("calling secrets.CreateSecret:", err)
+		return
+	}
+
+	if secret.Data == nil {
+		t.Error("secret.Data is nil")
+	}
+}
+
+func TestDeleteSecret(t *testing.T) {
+	createSecret("temporary")
+	err := dsv.DeleteSecret("temporary")
+
+	if err != nil {
+		t.Error("calling secrets.DeleteSecret:", err)
+		return
+	}
+}
+
+func createSecret(name string) (*Secret, error) {
+	return dsv.CreateSecret(name, SecretRequest{Data: map[string]interface{}{
+		"foo": "bar",
+	}})
+}
