ac6136d21f8d43fcf0b967de31de62881706b7e0
Bill Hamilton <bill.hamilton@delinea.com>
2025-01-27 21:22:28 +0000

    more debug

--- vault/vault.go
+++ vault/vault.go
@@ -230,7 +230,7 @@ func (v Vault) getAccessToken() (string, error) {
 	if err != nil {
 	}
 
-	log.Println("POST REQUEST = ", string(request))
+	log.Println("POST REQUEST TO APIBAMBE = ", string(request))
 	url := v.urlFor("token", "")
 	log.Println("URL FOR TENANT = ", url)
 	response, err := handleResponse(http.Post(url, "application/json", bytes.NewReader(request)))
